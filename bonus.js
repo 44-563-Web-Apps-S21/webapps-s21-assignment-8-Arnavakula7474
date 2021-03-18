e4b029958e0def47411dfa88e63c81e5b916eb6b
Arnavakula7474 <77798904+Arnavakula7474@users.noreply.github.com>
2021-03-18 00:30:23 -0500

    code for assignment 8

---
+++ bonus.js
@@ -0,0 +1,36 @@
+const http = require('http')    //Pull in a useful node package
+const hostname = process.env.hostname || '127.0.0.1'    //get our ip address from the environment
+const port = process.env.port || 3001               //and the port
+const server =
+    http.createServer(            //Creates the response loop
+        (req, res) => {               //Anonymous function to handle the request
+            console.log("Request recieved")
+            // console.log(req.url)
+            // Build a fake url so we can get the search parameters using a URL object
+            fake_url = "https://fake.com/path" + req.url
+            const url = new URL(fake_url)
+            if (req.method === 'GET') {
+
+                var jokes = ["hi", "my", "name", "is", "arnav", "akula"]
+                let randomIndex = Math.floor(Math.random() * jokes.length)
+                // Process the queries here
+                res.statusCode = 200      //code for OK
+                res.setHeader('Content-Type', 'text/plain')
+                res.write("yours")
+                res.write(jokes[randomIndex])
+                console.log(randomIndex)
+                console.log(jokes[randomIndex])
+                res.end();
+
+            } else {
+                console.log("Status 404")
+                res.statusCode = 404;
+                res.end();
+            }
+
+        }
+    )
+
+server.listen(port, hostname, () => {   //Start the server
+    console.log(`Server running at http://${hostname}:${port}/`)  //Log the request
+})
