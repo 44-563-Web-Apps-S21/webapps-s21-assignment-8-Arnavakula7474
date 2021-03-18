79827543eb1ce62cd58afbe4d3711f99a2b42c87
Arnavakula7474 <77798904+Arnavakula7474@users.noreply.github.com>
2021-03-17 23:01:19 -0500

    modified app2

--- app2.js
+++ app2.js
@@ -10,10 +10,22 @@ const server =
       res.setHeader('Content-Type', 'text/html') //Set the mime type HTML
 
       res.write('<html> <head> <title> Served </title> </head>')
-      res.write('<body>')
+      res.write('<body><style>table, th, td {border: 2px solid black;border-collapse: collapse;}</style>')
       res.write('Content \n')
       res.write('More content \n')
-      res.write('Hello World')    
+      res.write('Hello World')
+      res.write('<table>')
+      res.write('<tr>')
+      res.write('<th>Places</th>')
+      res.write('<th>Time to spend</th>')
+      res.write('<th>Amount to spend</th>')
+      res.write('</tr>')
+      res.write('<tr>')
+      res.write('<td>Rajasthan</td>')
+      res.write('<td>3 days</td>')
+      res.write('<td>$150</td>')
+      res.write('</tr>')
+      res.write('<tr><td>Maharastra</td><td>3 days</td><td>$200</td></tr><tr><td>Andhra Pradesh</td><td>4 days</td><td>$300</td></tr><tr><td>Telangana</td><td>2 days</td><td>$340</td></tr></table>')
       res.end('</body></html>')
       //Close the response
     }                           
